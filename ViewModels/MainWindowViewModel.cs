71a84a74771692c75e223f0cb05c098c6a5a1869
PiotrKruk99-d <biuro@liberezo.pl>
2022-08-24 23:45:24 +0200

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace avaloniaCalculator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        //private int displayValue = 0;
+        private double displayValue = 0;
+
+        // public string DisplayValue
+        // {
+        //     get => displayValue.ToString();
+        //     private set => this.RaiseAndSetIfChanged(ref displayValue, Convert.ToInt32(value));
+        // }
+
+        public double DisplayValue
+        {
+            get => displayValue;
+            private set => this.RaiseAndSetIfChanged(ref displayValue, value);
+        }
+
+        public void NumberSelected(string numberText)
+        {
+            double number = Convert.ToDouble(numberText);
+            //DisplayValue = (displayValue * 10 + number).ToString();
+            DisplayValue = displayValue * 10 + number;
+        }
+
+        public void ResetAll()
+        {
+            //DisplayValue = "0";
+            DisplayValue = 0;
+        }
+
+        public void ButtonClicked(string text)
+        {
+
+        }
+    }
+}
